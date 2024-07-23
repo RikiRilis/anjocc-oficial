7c2c5276d7dc2bbbe93a4f4e8e19256125048049
RikiRilis <thewhitzip@gmail.com>
2024-07-23 01:41:04 -0400

    Configuration files configured

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,34 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import react from '@astrojs/react'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [tailwind(), react()],
+	output: 'server',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+	i18n: {
+		defaultLocale: 'es',
+		locales: ['es', 'en'],
+		routing: {
+			prefixDefaultLocale: false,
+		},
+	},
+	vite: {
+		define: {
+			'import.meta.env.EMAILJS_KEY': JSON.stringify(process.env.EMAILJS_KEY),
+			'import.meta.env.EMAILJS_SERVICE_ID': JSON.stringify(process.env.EMAILJS_SERVICE_ID),
+			'import.meta.env.EMAILJS_TEMPLATE_ID': JSON.stringify(process.env.EMAILJS_TEMPLATE_ID),
+		},
+	},
+})
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="astro/client" />
+
+import type { ToastOptions } from '@/lib/toast'
+
+interface Window {
+	toast({ ToastOptions }: ToastOptions): void
+}
+
+interface ImportMetaEnv {
+	readonly EMAILJS_KEY: string
+	readonly EMAILJS_SERVICE_ID: string
+	readonly EMAILJS_TEMPLATE_ID: string
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
+}
+
+declare global {
+	interface Window {
+		toast: function
+	}
+}
